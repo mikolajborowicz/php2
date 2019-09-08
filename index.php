ecf6ebf540dcdcb1eca7819be704bbc8ac45af7c
Mikołaj <39616250+mikolajborowicz@users.noreply.github.com>
2019-09-08 12:35:42 +0200

    zegar logowania

--- index.php
+++ index.php
@@ -6,8 +6,7 @@
 	{
 		header('Location: gra.php');
 		exit();
-	}+
-
+	}
 ?>
 
 <!DOCTYPE html>
@@ -18,12 +17,45 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Osadnicy - gra przeglądarkowa</title>
     <link rel="stylesheet" href="main.css">
-    <link rel="stylesheet" href="script.js">
+        <!-- javascript -->
+
+        <script>
+        var dzisiaj = new Date();
+        
+        var dzien = dzisiaj.getDate();
+        var miesiac = dzisiaj.getMonth()+1;
+        if(miesiac<10){
+            miesiac="0"+miesiac;
+        }
+        var rok = dzisiaj.getFullYear();
+        var godzina = dzisiaj.getHours();
+        if(godzina<10){
+            godzina="0"+godzina;
+        }
+        var minuta = dzisiaj.getMinutes();
+        if(minuta<10){
+            minuta="0"+minuta;
+        }
+        var sekunda = dzisiaj.getSeconds();
+        if(sekunda<10){
+            sekunda="0"+sekunda;
+        }
+        </script>
 </head>
 <body>
     
 <h2>Tylko martwi ujrzeli koniec wojny - Platon</h2><br>
 <div id="zegar"></div>
+
+
+
+<script type="text/javascript">
+ document.getElementById("zegar").innerHTML = dzien+"/"+miesiac+"/"+rok+" | "+godzina+":"+minuta+":"+sekunda;
+</script>
+
+
+
+
 
 <a href="rejestracja.php">Zarejestruj się</a>
     <!-- frormularz wysyłający w paczce login i haslo do panel.php -->
--- script.js
+++
@@ -1,9 +0,0 @@
-var dzisiaj = new Date();
-//pobieranie daty i czsu lokalnego 
-var dzien = dzisiaj.getDate();
-var miesiac = dzisiaj.getMonth()+1;
-var rok = dzisiaj.getFullYear();
-
-var godzina = dzisiaj.getHours();
-var minuta = dzisiaj.getMinutes();
-var sekunda = dzisiaj.getSeconds();
